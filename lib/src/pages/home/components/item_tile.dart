e85b421522c010f4434343b8f2844c07768cf6b0
Guilherme Lellis <luis.lellis@yandeh.com.br>
2023-07-03 22:08:22 -0300

    feat: add hero animation

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:goya/src/config/custom_colors.dart';
 import 'package:goya/src/models/item_model.dart';
+import 'package:goya/src/pages/product/product_screen.dart';
 import 'package:goya/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
@@ -17,53 +18,68 @@ class ItemTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Card(
-          elevation: 1,
-          color: Colors.white,
-          shadowColor: Colors.grey.shade100,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // Imagem
-                Expanded(
-                  child: Image.asset(item.imgUrl),
-                ),
-                // Nome
-                Text(
-                  item.itemName,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                // Preço - Unidade
-                Row(
-                  children: [
-                    Text(
-                      utilsServices.priceToCurrency(item.price),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: CustomColors.customSwatchColor,
-                      ),
+        // Conteudo
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (c) {
+              return ProductScreen(
+                item: item,
+              );
+            }));
+          },
+          child: Card(
+            elevation: 1,
+            color: Colors.white,
+            shadowColor: Colors.grey.shade100,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Imagem
+                  Expanded(
+                    child: Hero(
+                      tag: item.imgUrl,
+                      child: Image.asset(item.imgUrl),
                     ),
-                    Text('/${item.unit}',
+                  ),
+                  // Nome
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  // Preço - Unidade
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
                         style: TextStyle(
-                          color: Colors.grey.shade500,
                           fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ))
-                  ],
-                ),
-              ],
+                          fontSize: 20,
+                          color: CustomColors.customSwatchColor,
+                        ),
+                      ),
+                      Text('/${item.unit}',
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ))
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+
+        // Botão add carrinho
         Positioned(
             top: 4,
             right: 4,
