2178efaba8ca7d07108cb5134971b510efbd120d
Guilherme Lellis <luis.lellis@yandeh.com.br>
2023-07-03 21:33:24 -0300

    feat: finished tile component

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,63 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:goya/src/config/custom_colors.dart';
 import 'package:goya/src/models/item_model.dart';
+import 'package:goya/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({
+  final ItemModel item;
+
+  ItemTile({
     super.key,
     required this.item,
   });
 
-  final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      color: Colors.white,
-      shadowColor: Colors.grey.shade100,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Imagem
-            Expanded(
-              child: Image.asset(item.imgUrl),
-            ),
-            // Nome
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            // Preço - Unidade
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          color: Colors.white,
+          shadowColor: Colors.grey.shade100,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                // Imagem
+                Expanded(
+                  child: Image.asset(item.imgUrl),
+                ),
+                // Nome
                 Text(
-                  item.price.toString(),
-                  style: TextStyle(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: CustomColors.customSwatchColor,
                   ),
                 ),
-                Text('/${item.unit}',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                    ))
+                // Preço - Unidade
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text('/${item.unit}',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ))
+                  ],
+                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+            top: 4,
+            right: 4,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                width: 35,
+                decoration: BoxDecoration(
+                    color: CustomColors.customSwatchColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      topRight: Radius.circular(20),
+                    )),
+                child: const Icon(
+                  Icons.add_shopping_cart_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ))
+      ],
     );
   }
 }
